85f60ef04fe1e711228db227acbe6a3cfd2c3c3d
karthikeya-1734 <ak.karthikeya.kk@gmail.com>
2024-05-28 13:28:06 +0530

    Bidirectional chatting simultaneously

--- src/Client.java
+++ src/Client.java
@@ -1,21 +1,43 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class Client {
+public class Client{
     public static void main(String[] args) throws UnknownHostException, IOException {
-        
-        Socket socket = new Socket("localhost",1111);
-        
-        while (true) {
-            DataInputStream din = new DataInputStream(socket.getInputStream());
-            String msg = din.readUTF();
-            System.out.println("Server : "+msg);
-    
-            
-        }
+
+        Socket socket = new Socket("localhost",9999);
+
+        DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+        DataInputStream din = new DataInputStream(socket.getInputStream());
+        Scanner sc = new Scanner(System.in);
+        new Thread(()->{
+            while (true) {
+                try {
+                    String msg;
+                    System.out.print("You: ");
+                    msg = sc.next();
+                    dout.writeUTF(msg);
+                    dout.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }                    
+            }
+        }).start();
+
+        new Thread(()->{
+            while (true) {
+                try {
+                    String msg = din.readUTF();
+                    System.out.println("\n Message From Server : "+msg);
+                    System.out.print("You: ");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }                    
+            }
+        }).start();
         
     }
-
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -5,18 +6,44 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server{
-    public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-
-        ServerSocket serversocket = new ServerSocket(1111);
+    public static void main(String[] args) throws IOException{
+        ServerSocket serversocket = new ServerSocket(9999);
+        System.out.println("Server Waiting...");
         Socket socket = serversocket.accept();
+        System.out.println("Connection Successful!");
 
-        while (true) {
-            System.out.println("You: ");
-            String msg = sc.next();
-            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-            dout.writeUTF(msg);
-            dout.flush();    
-        }
+        DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+        DataInputStream din = new DataInputStream(socket.getInputStream());
+
+        Scanner sc = new Scanner(System.in);
+        new Thread(()->{
+            while (true) {
+                try {
+                    String msg;
+                    System.out.print("You: ");
+                    msg = sc.next();
+                    dout.writeUTF(msg);
+                    dout.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }                    
+            }
+        }).start();
+
+        new Thread(()->{
+            while (true) {
+                try {
+                    String msg = din.readUTF();
+                    System.out.println("\n Message From Client : "+msg);
+                    System.out.print("You: ");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }                    
+            }
+        }).start();
+
+
     }
+
+    
 }
